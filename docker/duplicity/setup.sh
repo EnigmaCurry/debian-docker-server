d587f7473eb419e67fe335ffd53a66f5a923cc02
Ryan McGuire <ryan@enigmacurry.com>
2016-03-24 15:55:14 -0400

    Require encryption passphrase

--- docker/duplicity/setup.sh
+++ docker/duplicity/setup.sh
@@ -19,14 +19,12 @@ do
     [ -z "$AWS_BUCKET" ] && read -p "Please enter your AWS Bucket Name: " AWS_BUCKET
     [ -z "$AWS_ACCESS_KEY" ] && read -p "Please enter your AWS Access Key: " AWS_ACCESS_KEY
     [ -z "$AWS_SECRET_KEY" ] && read -p "Please enter your AWS Secret Key: " AWS_SECRET_KEY
-    confirm_vars AWS_BUCKET AWS_ACCESS_KEY AWS_SECRET_KEY
+    [ -z "$AWS_ENCRYPTION_KEY" ] && read -p "Please enter a passphrase to encrypt data: " AWS_ENCRYPTION_KEY
+    confirm_vars AWS_BUCKET AWS_ACCESS_KEY AWS_SECRET_KEY AWS_ENCRYPTION_KEY
     [ $? == 0 ] && break
 done
 
-EXTRA_DOCKER_OPTS="-e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_KEY -e REMOTE_URL=s3+http://$AWS_BUCKET/ -e SOURCE_PATH=/backups"
-if [ -v "AWS_ENCRYPTION_KEY" ]; then
-    EXTRA_DOCKER_OPTS=$EXTRA_DOCKER_OPTS" -e PASSPHRASE=$AWS_ENCRYPTION_KEY"
-fi
+EXTRA_DOCKER_OPTS="-e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_KEY -e REMOTE_URL=s3+http://$AWS_BUCKET/ -e SOURCE_PATH=/backups -e PASSPHRASE=$AWS_ENCRYPTION_KEY"
 
 THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 BACKUP_VOLUME=$THIS_DIR/../../docker_volumes
