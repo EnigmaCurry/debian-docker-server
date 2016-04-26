817421fd92066d94e3506beb1b73c28bb1411ec0
Ryan McGuire <ryan@enigmacurry.com>
2016-04-26 18:08:24 -0400

    Switch from apt to apt-get
    apt is the new hotness, but not supposed to use in scripts

--- debian_setup.sh
+++ debian_setup.sh
@@ -18,13 +18,13 @@ if [[ $EUID != 0 ]]; then
     exit 1
 fi
 
-exe apt update
-exe apt install -y apt-transport-https ca-certificates
+exe apt-get update
+exe apt-get install -y apt-transport-https ca-certificates
 
-exe apt install -y git emacs-nox
+exe apt-get install -y git emacs-nox
 
 # Initial firewall rules:
-exe apt install ufw
+exe apt-get install -y ufw
 yes | ufw enable
 exe ufw default deny
 exe ufw allow 22
@@ -35,8 +35,8 @@ apt-get install -y fail2ban
 # Install Docker
 exe apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 echo "deb https://apt.dockerproject.org/repo debian-jessie main" > /etc/apt/sources.list.d/docker.list
-exe apt update
-exe apt install -y docker-engine
+exe apt-get update
+exe apt-get install -y docker-engine
 exe systemctl start docker
 
 export DDS_ROOT=$PWD/debian-docker-server
